691f2f0f31aad74fbaee9f2d9be8423a87fdc059
Killian Viquerat <killian.viquerat@cpnv.ch>
2019-10-31 08:59:58 +0100

    Visit Factory
    Modification Date to be between now and one month in the future.
    Modification of visitsstates id to take a random one 
    instead of a hardcode one.

--- database/factories/VisitFactory.php
+++ database/factories/VisitFactory.php
@@ -9,17 +9,18 @@ Factory define to add data to the table visit
 $factory->define(App\Visit::class, function (Faker $faker) {
 
     return [
-        'moment' => $faker->date('Y-m-d H:i:s'),
+        'moment' => $faker->dateTimeBetween('now','+1 month'),
         'confirmed' => 1,
         'number' => $faker->randomNumber(1),
-        'grade' => $faker->randomFloat(1,1,6),
+        'grade' => $faker->randomFloat(0,1,6),
         'mailstate' => 1,
         'internships_id' => function () {
-            $internships=App\Internship::all();
-            $numberInternships=count($internships);
-            $randomNumber= random_int(0,$numberInternships);
-            return $internships[$randomNumber]->id;
+            $internships=App\Internship::all()->random()->id;
+            return $internships;
         },
-        'visitsstates_id' => 1,
+        'visitsstates_id' => function (){
+            $visitsstates=App\Visitsstate::all()->random()->id;
+            return $visitsstates;
+        },
     ];
 });
