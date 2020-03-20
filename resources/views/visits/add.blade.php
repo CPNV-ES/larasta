d5dd0782dbcd5a65ad252c81153d8e90d6d9b6fc
diogovieiraferreira <diogo.vieira-ferreira@cpnv.ch>
2020-03-20 16:10:31 +0100

    update subview add visit

--- resources/views/visits/add.blade.php
+++ resources/views/visits/add.blade.php
@@ -1,29 +1,42 @@
 
 <h1>Créer une nouvelle visite</h1>
+@if($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
 <div class="container">
     <div class="col-md-4 col-md-offset-4">
-        <form method="post" action="{{ route( 'InternshipsAddVisit' , [ 'iid' => $internship ]) }}">
+        <form method="post" action="{{ route( 'visite.create' , [ 'id' => $internship ]) }}">
             {{ csrf_field() }}
             <table>
                 <thead>
                     <th>N° visite</th>
+                    <th>Jour</th>
+                    <th>Heure</th>
                     <th>Mail envoyé?</th>
                     <th>Confirmé?</th>
                     <th>Note</th>
                     <th>État de la visite</th>
+                    <th>Créer</th>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><input type="number" min="1" name="number"/></td>
-                        <td><input type="checkbox" name="mailstate" /></td>
-                        <td><input type="checkbox" name="confirmed" /></td>
-                        <td><input type="number" min="1" max="6" name="grade"/></td>
+                        <td><input type="number" min="1" name="number" value="1" required/></td>
+                        <td><input type="date" name="day"/></td>
+                        <td><input type="time" name="hour"/></td>
+                        <td><input type="checkbox" name="mailstate"/></td>
+                        <td><input type="checkbox" name="confirmed"/></td>
+                        <td><input type="number" min="1" max="6" step="0.5" name="grade" value="1" required/></td>
                         <td>
-                            <select name="visitestate_id">
+                            <select name="visitsstates_id" required>
                                 @foreach ($visitsStates as $visitstate)
                                     <option value="{{$visitstate->id}}">{{ $visitstate->stateName }}</option>                                    
                                 @endforeach
                             </select>
+                        </td>
+                        <td>
+                            <button type="submit">créer</button>
                         </td>
                     </tr>
                 </tbody>
