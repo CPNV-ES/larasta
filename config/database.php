c131f048347f28fbd25cfb2cf0c22cbf8548ac29
Killian Viquerat <kil.viquerat@gmail.com>
2020-03-27 00:53:44 +0100

    Install & Config Capistrano

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
