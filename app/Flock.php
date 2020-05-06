44eccaf10387d186bbbf1a53a13fd485a0f4da25
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-05-06 23:03:16 +0200

    Get flocks on internships

--- app/Flock.php
+++ app/Flock.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Flock extends Model
@@ -23,5 +24,48 @@ class Flock extends Model
     public function classMaster ()
     {
         return $this->belongsTo('App\Person', 'classMaster_id');
+    }
+
+    public function getYearsOfFlocksOnInternship()
+    {
+        $today = Carbon::now();
+        //interships of the flock of the previous year 
+        $augustPreviousYear = new Carbon('first day of august');
+        $augustPreviousYear->subYear(1);
+        $january = new Carbon('last day of january');
+
+        //interships of flocks of the current and previous year 
+        $february = new Carbon('first day of february');
+        $july = new Carbon('last day of july');
+
+        //interships of the flock of the current year 
+        $august = new Carbon('first day of august');
+        $januaryNextYear = new Carbon('last day of january');
+        $januaryNextYear->addYear(1);
+
+                 
+        $previousYear = Carbon::now()->subYear(1)->year ;
+        $actualYear = Carbon::now()->year ;
+        
+        $flocksInInternship = array( $actualYear );
+
+        if ( $today->between($augustPreviousYear, $january) )
+            $flocksInInternship = array($previousYear);
+        else 
+            array_push( $flocksInInternship, $previousYear );
+
+        return $flocksInInternship;
+    }
+
+    public function getFlockHasToGoOnInternships()
+    {
+        $flocksInInternship = $this->getYearsOfFlocksOnInternship();
+        if(count($flocksInInternship)>1)
+            $listOfFlocks = self::where('startYear',substr($flocksInInternship[0],2,4))->orwhere('startYear',substr($flocksInInternship[1],2,4))->get();
+        else
+            $listOfFlocks = self::where('startYear',substr($flocksInInternship[0],2,4))->get();
+            dd($listOfFlocks);
+            
+        return $flocksInInternship;
     }
 }
