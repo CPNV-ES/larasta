3ec89e8b1fa6c6b4854dc13568bb825471b2caa5
Nicolas Maitre <nmaitre@z-nm-06080800.cpnv.ch>
2020-06-10 15:57:34 +0200

    encapsulated internships script

--- resources/assets/js/internships.js
+++ resources/assets/js/internships.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    var converter = new showdown.Converter({tables: true});
-    var text = description.innerHTML;
-    var html = converter.makeHtml(text);
-    console.log(html);
-    description.innerHTML = html;
-});
+class InternshipUtils {
+    constructor() {
+        document.addEventListener("DOMContentLoaded", this._onload);
+    }
+    _onload() {
+        this.renderMarkdown(description);
+        this.reloadStudents();
+    }
+    renderMarkdown(element) {
+        var converter = new showdown.Converter({ tables: true });
+        var text = element.innerHTML;
+        var html = converter.makeHtml(text);
+        element.innerHTML = html;
+    }
+    reloadStudents(){
+        
+    }
+}
+var internshipUtils = new InternshipUtils();
