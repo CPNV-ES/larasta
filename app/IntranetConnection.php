9700fe70bcb54008f2fc641ba31b2f0cb732e9e4
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-06-19 22:43:53 +0200

    get data from api and sort it, first version

--- app/IntranetConnection.php
+++ app/IntranetConnection.php
@@ -18,9 +18,9 @@ class IntranetConnection
      * $teachersList : This attribute will contain the list of teachers returned by the intranet
      * $classesList : This attribute will contain the list of classes returned by the intranet
      */
-    private $studentsList;
-    private $teachersList;
-    private $classesList;
+    private $students;
+    private $teachers;
+    private $classes;
 
     /**
      * generateSignature
@@ -52,13 +52,15 @@ class IntranetConnection
     public function __construct()
     {
         /// Create an associative array with the url and their parameters to create the signature
-        $urlArray = ["http://intranet.cpnv.ch/info/etudiants.json?alter[extra]=current_class&api_key=" => "alter[extra]current_classapi_key",
-                     //"http://intranet.cpnv.ch/info/enseignants.json?api_key=" => "api_key",
-                     "http://intranet.cpnv.ch/info/enseignants.json?alter[extra]=current_class_masteries&api_key=gifi&signature=40766a439a0d749fa838a44c74341781",
-                     "http://intranet.cpnv.ch/info/classes.json?api_key=" => "api_key"];
-
+        $urlArray = ["http://intranet.cpnv.ch/info/classes.json?api_key=" => "api_key",
+                     "http://intranet.cpnv.ch/info/etudiants.json?alter[extra]=current_class&api_key=" => "alter[extra]current_classapi_key",
+                     "http://intranet.cpnv.ch/info/enseignants.json?alter[extra]=current_class_masteries&api_key=gifi&signature=40766a439a0d749fa838a44c74341781" => "alter[extra]current_class_masteriesapi_key"
+        ];
+        //TODO Faire un tableau avec toutes les classe 3ème année,
+        //Ensuite, mettre dans ["students"] tous les étudiants de cette classe
+        //pareil pour les enseignants, les mettre dans ["teachers"]
+        //puis retourner 1 seul tableau contenant toutes les infos utiles
         $connection = curl_init();
-
         foreach ($urlArray as $url => $urlSign)
         {
             curl_setopt_array($connection, [
@@ -77,54 +79,79 @@ class IntranetConnection
             $data = json_decode($response, true);
             if (strpos($url, "etudiants") !== false)
             {
-                foreach($data as $keys => $value)
-                {
-                    $name = $data[$keys]["current_class"]["link"]["name"];
-                    //keep only students on SI-C3a, SI-C3b, SI-MI3a, ...
-                    if(!preg_match("#^SI-\w+3\w+$#", $name))
-                        unset($data[$keys]);
-                }
-                array_values($data);
-
-                $this->students = $data;
+                $this->students = $this->sortStudentsByClasses($data);
             }
             else if (strpos($url, "enseignants") !== false)
             {
-                foreach($data as $keys => $value)
-                {
-                    $isValid = false;
-                    $classes = $data[$keys]["current_class_masteries"];
-                    foreach($classes as $keys => $value)
-                    {
-                        if(preg_match("SI-\w+3\w+", $classes["link"]["name"]))
-                            $isValid=true;
-                    }                    
-                    //keep only students on SI-C3a, SI-C3b, SI-MI3a, ...
-                    if(!$isValid)
-                        unset($data[$keys]);                    
-                }
-                array_values($data);
-                $this->teachers = $data;
-            }
-            else if (strpos($url, "classes") !== false)
-            {
-                foreach($data as $keys => $value)
-                {
-                    $name = $data[$keys]["name"];
-                    //keep only students on SI-C3a, SI-C3b, SI-MI3a, ...
-                    if(!preg_match("SI-\w+3\w+", $name))
-                        unset($data[$keys]);
-                }
-                array_values($data);
-                $this->classes = $data;
-            }
-            
-            dd("teachers",$this->teachersList, "students", $this->studentsList);
+                $this->teachers = $this->sortTeachersByClasses($data);
+            }            
         }
 
         curl_close($connection);
     }
 
+    function sortStudentsByClasses($json){
+        $regex = "#^SI-\w+3\w+$#";
+        
+        $studentsByClass=[];
+
+        foreach($json as $key => $value)
+        {
+            $name = $json[$key]["current_class"]["link"]["name"];
+            
+            //keep only students on SI-C3a, SI-C3b, SI-MI3a, ...
+            if(preg_match($regex, $name))
+            {
+                if(!isset($studentsByClass[$name]))
+                    $studentsByClass[$name] = array();
+                array_push($studentsByClass[$name],$json[$key]);
+            }
+        }
+
+        return $studentsByClass;
+    }
+
+    function sortTeachersByClasses($json){
+        $regex = "#^SI-\w+3\w+$#";
+        
+        $teachersByClasses=[];
+        $className=[];
+
+        foreach($json as $key => $value)
+        {
+            $isValid = false;
+            $classes = $json[$key]["current_class_masteries"];                        
+            foreach($classes as $classKey => $classValue)
+            {
+                //when a teacher as a match with the name of class, we keep
+                //the teacher, but we remove all unmatch classes
+                $className[$classKey] = array("id" => $classes[$classKey]["link"]["id"],
+                                              "name" => $classes[$classKey]["link"]["name"]);
+                if(preg_match($regex, $className[$classKey]["name"]))
+                    $isValid=true;
+                else
+                {
+                    //remove classes not matching with
+                    unset($json[$key]["current_class_masteries"][$classKey]);
+                    unset($className[$classKey]);
+                }
+            }                    
+            //keep only teachers on SI-C3a, SI-C3b, SI-MI3a, ...
+            if($isValid)
+            {
+                foreach($className as $classKey => $classData)
+                {
+                    $name = $classData["name"];
+                    $id = $classData["id"];
+                    $json[$key]["teacher_id"] = $json[$key]["id"];
+                    $json[$key]["class_id"] = $id;
+                    $teachersByClasses[$name] = $json[$key];
+                    unset($teachersByClasses[$name]["id"]);
+                }                
+            }        
+        }
+        return $teachersByClasses;
+    }
     /**
      * getStudents
      * 
@@ -134,7 +161,7 @@ class IntranetConnection
      */
     public function getStudents()
     {
-        return $this->studentsList;
+        return $this->students;
     }
 
     /**
@@ -146,7 +173,7 @@ class IntranetConnection
      */
     public function getTeachers()
     {
-        return $this->teachersList;
+        return $this->teachers;
     }
 
     /**
@@ -158,6 +185,6 @@ class IntranetConnection
      */
     public function getClasses()
     {
-        return $this->classesList;
+        return $this->classes;
     }
 }
