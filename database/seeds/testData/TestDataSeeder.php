5341cf433b9ec784f3fb1cee7836b76b872be9da
Killian Viquerat <kil.viquerat@gmail.com>
2020-06-25 21:29:06 +0200

    Delete call in TestDataSeeder

--- database/seeds/testData/TestDataSeeder.php
+++ database/seeds/testData/TestDataSeeder.php
@@ -25,7 +25,6 @@ class TestDataSeeder extends Seeder
         $this->call(LogbooksTableSeeder::class);
         $this->call(RemarksTableSeeder::class);
         $this->call(WishesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
         Schema::enableForeignKeyConstraints();
     }
 }
