69a259df827bdf923ec8c3c5c54669d644402ca3
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-02-02 15:06:59 +0100

    Have a better evaluation grid view

--- resources/views/evaluationgrid/index.blade.php
+++ resources/views/evaluationgrid/index.blade.php
@@ -4,20 +4,23 @@
     <h2>Sections</h2>
     <table class="larastable">
         <tr>
-            @forelse ($evaluationSections as $evaluationSection)
-                <th>{{ $evaluationSection->sectionName }}</th>
-                @forelse ($evaluationSection->criterias as $criteria)
-                <tr>
-                    <td>{{ $criteria->criteriaName }}</td>
-                </tr>
-                @empty 
-                <tr>
-                    <td>Aucun critère.</td>
-                </tr>
-            @endforelse 
-            @empty
-                <th>Aucune section.</th>
-            @endforelse  
+            <th class="text-success text-center">Sections</th>
+            <th class="text-success text-center">Critères</th>
+            <th class="text-success text-center">Colonnes</th>
         </tr>
+        @forelse ($evaluationSections as $evaluationSection)
+            <tr>
+                <td>{{ $evaluationSection->sectionName }}</td>
+                <td>
+                    @forelse ($evaluationSection->criterias as $criteria)
+                        - {{ $criteria->criteriaName }} <br>
+                    @empty
+                        Aucun critère.
+                    @endforelse
+                </td>        
+            </tr>        
+        @empty
+        <h2>Aucune section.</h2>
+        @endforelse 
     </table>
 @endsection
