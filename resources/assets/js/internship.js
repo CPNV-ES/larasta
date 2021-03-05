504cf1efaa5d3af0574864d4dc606765bb00f7ad
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-03-05 11:34:41 +0100

    Have a better code readability

--- resources/assets/js/internship.js
+++ resources/assets/js/internship.js
@@ -1,53 +1,52 @@
 class InternshipUtils {
-    constructor() {
-        document.addEventListener("DOMContentLoaded", ()=>{this._onload()});
-    }
-    _onload() {
-        this.renderMarkdown(description);
-    }
-    renderMarkdown(element) {
-        var converter = new showdown.Converter({ tables: true });
-        var text = element.innerHTML;
-        var html = converter.makeHtml(text);
-        element.innerHTML = html;
-    }
+  constructor() {
+    document.addEventListener("DOMContentLoaded", () => {
+      this._onload();
+    });
+  }
+  _onload() {
+    this.renderMarkdown(description);
+  }
+  renderMarkdown(element) {
+    var converter = new showdown.Converter({ tables: true });
+    var text = element.innerHTML;
+    var html = converter.makeHtml(text);
+    element.innerHTML = html;
+  }
 }
 var internshipUtils = new InternshipUtils();
 
 // -----------------------Filters----------------------- //
-var buttons = $("#filters").find(":checkbox")
-var allButton = $("#all")
-var oneButton = $("#one")
+var buttons = $("#filters").find(":checkbox");
+var allButton = $("#all");
+var oneButton = $("#one");
 
 // Show buttons on page load
-$(function () {
-    showButtons()
-})
+$(function() {
+  showButtons();
+});
 
 // Display the all or one button
-function showButtons () {
-    if ($("#filters").find(":checkbox:checked").length < buttons.length) {
-        allButton.parent().removeAttr("hidden")
-    }
+function showButtons() {
+  if ($("#filters").find(":checkbox:checked").length < buttons.length)
+    allButton.parent().removeAttr("hidden");
 }
 
-buttons.change(function () {    
-    showButtons()
+buttons.change(function() {
+  showButtons();
 
-    // The last filter button will always checked
-    if ($("#filters").find(":checkbox:checked").length < 1) {
-        this.checked = true
-    }
-})
+  // The last filter button will always checked
+  if ($("#filters").find(":checkbox:checked").length < 1) this.checked = true;
+});
 
-allButton.change(function () {
-    allButton.parent().attr("hidden", true)
-    this.checked = false
-    
-    // Check all buttons
-    buttons.each(function () {
-        this.checked = true
-    })
+allButton.change(function() {
+  allButton.parent().attr("hidden", true);
+  this.checked = false;
 
-    showButtons()
-})
+  // Check all buttons
+  buttons.each(function() {
+    this.checked = true;
+  });
+
+  showButtons();
+});
