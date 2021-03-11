b94a4c91d93d300e4c3e099c433a7a3a4ec30d25
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-03-11 09:42:15 +0100

    Updated remarkslist subview
    Updated the style to better fit requirements. Also, can now create a new remark for any remarkType

--- resources/views/remarks/remarkslist.blade.php
+++ resources/views/remarks/remarkslist.blade.php
@@ -1,18 +1,49 @@
 {{-- Subview to list remarks --}}
 {{-- Usage:     @include ('remarks.remarkslist',['remarks' => $your_array_of_remarks])  --}}
-<table class="table table-bordered col-md-10 larastable remarksTable">
+<form action="/remarks/create" method="get">
+@csrf
+<input type="hidden" name="remarkType" value="{{ $remarkType ?? ''  }}" />
+<input type="hidden" name="remarkOn_id" value="{{ $remarkOnId ?? '' }}" />
+<table class="table table-bordered col-md-12 larastable remarksTable">
     <tr>
-        <th class="col-md-1">Date</th>
-        <th class="col-md-1">Auteur</th>
-        <th class="col-md-8">Contenu</th>
+        <th colspan="4">Remarques</th>
     </tr>
     <tbody>
+
+    @if($edit ?? false)
+    <tr id="newRemarkBtnRow">
+        <td colspan="4">
+            <button class="btn btn-primary" type="button" onclick="remarks();">Ajouter une remarque</button>
+            <script type="text/javascript">
+                function remarks() {
+                    document.getElementById("newRemarkBtnRow").setAttribute("hidden", "true");
+                    document.getElementById("newRemarkFormRow").removeAttribute("hidden");
+                }
+            </script>
+        </td>
+    </tr>
+    <tr id="newRemarkFormRow" hidden>
+        <td>
+            <input name='remarkDate' type='date' value='{{ date("Y-m-d") }}' readonly required/>
+        </td>
+        <td>
+            <input name='remarkAuthor' type='text' value='{{ Auth::user()->initials }}' readonly required/>
+        </td>
+        <td>
+            <textarea name='remarkBody' required cols='100'></textarea>
+        </td>
+        <td>
+            <button class='btn btn-warning' type='submit'>Valider la remarque</button>
+        </td>
+    </tr>
+    @endif
+
     @foreach($remarks as $remark)
-    <tr class="fake-link" data-href="{{route("remarks.edit",$remark->id)}}">
-            <td class="col-md-1">{{ (new DateTime($remark->remarkDate))->format('d.M.y') }}</td>
-            <td class="col-md-1">{{ $remark->author }}</td>
-            <td class="col-md-8 text-left">{{ $remark->remarkText }}</td>
+            <td>{{ (new DateTime($remark->remarkDate))->format('d M y') }}</td>
+            <td>{{ $remark->author }}</td>
+            <td colspan="2">{{ $remark->remarkText }}</td>
         </tr>
     @endforeach
     </tbody>
 </table>
+</form>
