cee507de2ad7ff1a5be444389a748d10d793dc81
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-03-30 15:49:19 +0200

    Now display current eval grid template on evalgrid admin page

--- app/Http/Controllers/EvaluationGridController.php
+++ app/Http/Controllers/EvaluationGridController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Evaluation;
 use App\EvaluationSection;
 
 class EvaluationGridController extends Controller
 {
     public function index() 
     {
-        $evaluationSections = EvaluationSection::all();
+        // TODO: Can we do this with Eloquent?
+        // Get all unique evaluationSection in the current template evalgrid
+        $evaluationSections = [];
+        foreach(Evaluation::current_template()->criteriaValue as $criteriaValue) {
+            if (!in_array($criteriaValue->criteria->evaluationSection, $evaluationSections)) {
+                $evaluationSections[] = $criteriaValue->criteria->evaluationSection;
+            }
+        }
+
+        usort($evaluationSections, function($a, $b) { return $a->id > $b->id; });
 
         return view('evaluationgrid.index')->with(compact('evaluationSections'));
     }
