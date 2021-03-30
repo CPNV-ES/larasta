2ec9b108016219bed276524eadd48113e737c9e8
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-03-30 14:48:44 +0200

    Have a method to store the default sections

--- app/Http/Controllers/InternshipReportController.php
+++ app/Http/Controllers/InternshipReportController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\InternshipReport;
 use App\Internship;
+use App\ReportSection;
+use SebastianBergmann\CodeCoverage\Report\Xml\Report;
 
 class InternshipReportController extends Controller
 {
@@ -16,6 +18,8 @@ class InternshipReportController extends Controller
             $newReport->internship_id = $internshipId;
             $newReport->save();
 
+            $this->storeDefaultFields($newReport->id);
+
             return redirect()->route('internshipReport.show', ['id' => $newReport->id]);
         } else {
             abort(404);
@@ -26,5 +30,15 @@ class InternshipReportController extends Controller
     {
         $report = InternshipReport::findOrFail($id);
         return view('internshipreports.show', compact('report'));
+    }
+
+    private function storeDefaultFields($reportId)
+    {
+        $report = InternshipReport::find($reportId);
+        $report->sections()->createMany([
+            ["name" => "Description du contexte de mon stage (entreprise, domaine d'activité, rôles, etc...)"],
+            ["name" => "Description de mon plan de carrière"],
+            ["name" => "Description de mon équipe (mission, personnes, rôles, organisation, ...)"],
+        ]);
     }
 }
