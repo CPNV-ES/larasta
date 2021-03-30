15e43530389b004e4fe1449840169506430d5d08
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-03-30 14:49:25 +0200

    Refactor the view using foreach

--- resources/views/internshipreports/show.blade.php
+++ resources/views/internshipreports/show.blade.php
@@ -4,105 +4,33 @@
 <h2>Rapport de stage de {{ $report->internship->student->firstname }} {{ $report->internship->student->lastname }} chez
     {{$report->internship->company->companyName}}</h2>
 
-@if (count($report->sections) < 1) <section class="mb-5">
+@foreach ($report->sections as $section)
+<section class="mb-5">
     <form method="POST" action="{{route("internshipReport.store", $report->id)}}">
         @csrf
         <table class="table text-left larastable mt-5">
             <tr scope="row">
                 <td scope="col-md-2">Titre</td>
                 <td>
-                    <input id="title" type="text" name="title" class="w-100"
-                        value="Description du contexte de mon stage (entreprise, domaine d'activité, rôles, etc...)"
-                        readonly />
+                    <input id="title" type="text" name="title" class="w-100" value="{{$section->name}}" readonly />
                 </td>
             </tr>
             <tr scope="row">
                 <td>Description</td>
                 <td class="Description">
-                    <textarea id="description" name="description" class="w-100" readonly></textarea>
+                    <textarea id="description" name="description" class="w-100" value="{{$section->text}}"
+                        readonly></textarea>
                 </td>
             </tr>
         </table>
         <button id="edit" name="edit" type="button" class="btn-warning">Editer</button>
-        <button id="save" name="save" type="submit" class="btn-success save-button" hidden>Enregistrer</button>
+        <button id="save" name="save" type="submit" class="btn-success" hidden>Enregistrer</button>
     </form>
-    </section>
+</section>
+@endforeach
 
-    <section class="mb-5">
-        <form method="POST" action="{{route("internshipReport.store", $report->id)}}">
-            @csrf
-            <table class="table text-left larastable mt-5">
-                <tr scope="row">
-                    <td scope="col-md-2">Titre</td>
-                    <td>
-                        <input id="title" type="text" name="title" class="w-100"
-                            value="Description de mon plan de carrière" readonly />
-                    </td>
-                </tr>
-                <tr scope="row">
-                    <td>Description</td>
-                    <td class="Description">
-                        <textarea id="description" name="description" class="w-100" readonly></textarea>
-                    </td>
-                </tr>
-            </table>
-            <button id="edit" name="edit" type="button" class="btn-warning">Editer</button>
-            <button id="save" name="save" type="submit" class="btn-success" hidden>Enregistrer</button>
-        </form>
-    </section>
+@endsection
 
-    <section class="mb-5">
-        <form method="POST" action="{{route("internshipReport.store", $report->id)}}">
-            @csrf
-            <table class="table text-left larastable mt-5">
-                <tr scope="row">
-                    <td scope="col-md-2">Titre</td>
-                    <td>
-                        <input id="title" type="text" name="title" class="w-100"
-                            value="Description de mon équipe (mission, personnes, rôles, organisation, ...)" readonly />
-                    </td>
-                </tr>
-                <tr scope="row">
-                    <td>Description</td>
-                    <td class="Description">
-                        <textarea id="description" name="description" class="w-100" readonly></textarea>
-                    </td>
-                </tr>
-            </table>
-            <button id="edit" name="edit" type="button" class="btn-warning">Editer</button>
-            <button id="save" name="save" type="submit" class="btn-success" hidden>Enregistrer</button>
-        </form>
-    </section>
-    @else
-    @foreach ($report->sections as $section)
-    <section class="mb-5">
-        <form method="POST" action="{{route("internshipReport.store", $report->id)}}">
-            @csrf
-            <table class="table text-left larastable mt-5">
-                <tr scope="row">
-                    <td scope="col-md-2">Titre</td>
-                    <td>
-                        <input id="title" type="text" name="title" class="w-100" value="{{$section->name}}" readonly />
-                    </td>
-                </tr>
-                <tr scope="row">
-                    <td>Description</td>
-                    <td class="Description">
-                        <textarea id="description" name="description" class="w-100" value="{{$section->text}}"
-                            readonly></textarea>
-                    </td>
-                </tr>
-            </table>
-            <button id="edit" name="edit" type="button" class="btn-warning">Editer</button>
-            <button id="save" name="save" type="submit" class="btn-success" hidden>Enregistrer</button>
-        </form>
-    </section>
-    @endforeach
-
-    @endif
-
-    @endsection
-
-    @push ('page_specific_js')
-    <script src="/js/internshipreport.js"></script>
-    @endpush
+@push ('page_specific_js')
+<script src="/js/internshipreport.js"></script>
+@endpush
