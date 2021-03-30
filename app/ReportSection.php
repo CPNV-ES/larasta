e8a45096e6d882b2873f2fe20c295665afa9ca73
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-03-30 14:47:58 +0200

    Make the field name fillable

--- app/ReportSection.php
+++ app/ReportSection.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ReportSection extends Model
 {
     protected $table = 'reportsections';
+    protected $fillable = ['name'];
 
     public function report()
     {
