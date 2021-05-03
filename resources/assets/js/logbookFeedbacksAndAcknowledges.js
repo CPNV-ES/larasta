2be919404b217c96036ad6f62a319341a7d0c6a2
Yvann <yvann.butticaz@gmail.com>
2021-05-03 17:01:56 +0200

    update js for logbook

--- resources/assets/js/logbookFeedbacksAndAcknowledges.js
+++ resources/assets/js/logbookFeedbacksAndAcknowledges.js
@@ -1,5 +1,10 @@
 
 function showFeedbackFields(name) {
+    document.getElementById('save').removeAttribute("hidden");
     document.getElementById('btnFdbk'+name).setAttribute("hidden", "hidden");
     document.getElementById(name).removeAttribute("hidden");
+}
+
+function showSaveBtn(){
+    document.getElementById('save').removeAttribute("hidden");
 }
