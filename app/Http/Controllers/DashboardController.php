c7b6e88bf685cab1c9db4773183e76b1c413d37d
Yvann <yvann.butticaz@gmail.com>
2021-06-03 11:22:17 +0200

    Add function to get passedIntership, update getMyVisit to getClassMasterVisits

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -22,23 +22,27 @@ class DashboardController extends Controller
             //User is a Teacher
             if (Auth::user()->role >= 1){
                 $teachersInternships = $this->getMyInternships(Auth::user()->id);
-                $teachersVisits = $this->getMyVisits(Auth::user()->id);
-                return view('dashboard/dashboard')->with(['internships' => $teachersInternships, 'visits' => $teachersVisits]);
+                $teachersPastInternships = $this->getPastInterships(Auth::user()->id);
+                $teachersVisits = $this->getClassMasterVisits(Auth::user()->id);
+
+                return view('dashboard/dashboard')->with(['internships' => $teachersInternships, 'pastInternships' => $teachersPastInternships, 'visits' => $teachersVisits]);
                 
-            }
             //User is a student
-            elseif(Auth::user()->role == '0'){
+            }elseif(Auth::user()->role == '0'){
                 $studentInternships = $this->getMyInternships(Auth::user()->id);
-                return view('dashboard/dashboard')->with(['internships' => $studentInternships]);
-            }else{
                 
+                return view('dashboard/dashboard')->with(['internships' => $studentInternships]);
+
+            }else{
                 return redirect()->route('internships.index');
-            }              
+            }   
+
         }else{
             return view('dashboard/dashboard');
         }
     }
 
+    //Get user Interships
     public function getMyInternships($id){
         //Get Class Master's internships 
         if(Auth::user()->role == 1){
@@ -46,7 +50,7 @@ class DashboardController extends Controller
                 $query->where('classMaster_id',$id); 
             })->orderBy('beginDate', 'DESC')->get();
 
-        //Get Student's internships
+        //Get Student's internships with the concerned visits
         }elseif (Auth::user()->role == 0){
             $result = Internship::where('intern_id', $id)->with('visits')->get();
         }
@@ -54,14 +58,22 @@ class DashboardController extends Controller
         return $result;
     }
 
-    public function getMyVisits($id){
-        //Teacher
-        if(Auth::user()->role == 1){
-            //Eloquent query gets all the visits from teacher ID that are not in the state 'Bouclée'
-            $result = Visit::whereHas('internship.student.flock',function($query) use ($id){
-                $query->where('classMaster_id',$id)->where('visitsstates_id','!=', '4');
-            })->orderBy('moment', 'DESC')->get();
-        }
+
+    //Get Class Master's internships that are in contract state 'Effectué' 
+    public function getPastInterships($id){
+        $result = Internship::whereHas('student.flock',function($query) use ($id){
+            $query->where('classMaster_id',$id)->where('contractstate_id', '13'); 
+        })->orderBy('beginDate', 'DESC')->get();
+
+        return $result;
+    }
+
+    //Return the visit that concern the ClassMaster
+    public function getClassMasterVisits($id){
+        //Eloquent query gets all the visits from teacher ID that are not in the state 'Bouclée'
+        $result = Visit::whereHas('internship.student.flock',function($query) use ($id){
+            $query->where('classMaster_id',$id)->where('visitsstates_id','!=', '4');
+        })->orderBy('moment', 'DESC')->get();
         
         return $result;
     }
